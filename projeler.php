b7430e899ad45dc80957ad23cf2176961699ae1a
İbrahim Kangal <150201206@kocaeli.edu.tr>
2017-08-14 16:28:08 +0300

    Bilgisayar Mühendisliği Bölüm web sayfası
    php tabanlı admin paneli

---
+++ projeler.php
@@ -0,0 +1,297 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<title>Projeler</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+<link href="css/custom.css" rel="stylesheet" type="text/css" />
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script src="js/tut.js" type="text/javascript" charset="utf-8"></script>
+<script src="js/bootstrap-hover-dropdown.min.js"></script>
+<script type="text/javascript">$(document).ready(function(){$('.menu').scrollToFixed();});</script>
+
+</head>
+<body>
+<!-- üst header -->
+
+<div class="container-fluid" style="background-color:#36273c">
+	
+    	<div class="col-md-8">
+        <span style="color:#fff;float:left;padding-top:2px">Kocaeli Üniversitesi Bilgisayar Mühendisliği |  <a href="http://ogr.kocaeli.edu.tr/KOUBS/ogrenci/index.cfm" target="_blank" style="text-decoration: none; color: #fff">ÖBS  |  </a><a href="http://ogr.kocaeli.edu.tr/KOUBS/AkaPers/index.cfm" target="_blank" style="text-decoration: none; color: #fff">APS  |  </a><a href="http://ogr.kocaeli.edu.tr/KOUBS/Idaripers/anagiris.cfm" target="_blank" style="text-decoration: none; color: #fff">İPS</a> |  <a><a href="http://posta1.kocaeli.edu.tr/iwc_static/layout/login.html?lang=tr&3.0.0.0.1_020202&svcs=abs,mail,calendar" target="_blank" style="text-decoration: none; color: #fff">E-Posta</a></span>
+       
+        </div>
+        <div class="col-md-4 logos">
+           <div class="row right" >
+ 			<span style="color:#fff">Login</span><a href="login.php"  style="text-decoration: none; color: #fff"> <i class="glyphicon glyphicon-log-in"></i></a>           
+            </div>
+            
+            
+            
+       
+    </div>
+</div> 
+
+<!-- üst header -->
+
+<div class="container">
+<div class="menu">
+ <div class="row">
+			<nav class="navbar navbar-default" style="background-color:#E0F2B7" role="navigation">
+			  <div class="container-fluid">
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				</div>
+				<div class="collapse navbar-collapse acilirmenu" id="bs-example-navbar-collapse-1">
+				  <ul class="nav navbar-nav" style="margin-left:150px">
+					<li><a href="index.php">Anasayfa</a></li>
+					<li class="dropdown">
+					  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Hakkımızda <span class="caret"></span></a>
+					  <ul class="dropdown-menu" role="menu">
+						<li><a href="tarihce.php">Tarihçe</a></li>
+						<li><a href="hakkimizda.php">Hakkımızda</a></li>
+						<li><a href="misyonvizyon.php">Misyon / Vizyon</a></li>
+						<li><a href="anabilimdallari.php">Ana Bilim Dalları</a></li>
+						<li><a href="programciktilari.php">Program Çıktıları</a></li>
+                        <!--<li><a href="galeri.php">Fotoğraf Galerisi</a></li>  -->
+					  </ul>
+					</li>
+					<li><a href="yonetim.php">Yönetim</a></li>
+					<li class="dropdown">
+					  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Araştırma <span class="caret"></span></a>
+					  <ul class="dropdown-menu" role="menu">
+						<li><a href="http://bilgisayar.kocaeli.edu.tr/akilli_sistemler_lab/"  target="_blank">Akıllı Sistemler Araştırma Laboratuvarı</a></li>
+						<li><a href="http://bilgisayar.kocaeli.edu.tr/comnet" target="_blank">Bilgisayar Ağları ve Haberleşme Araştırma Laboratuvarı</a></li>
+						<li><a href="http://embedded.kocaeli.edu.tr/" target="_blank">Gömülü ve Algılayıcı Sistemler Araştırma Laboratuvarı</a></li>
+						<li><a href="http://ipcv.kocaeli.edu.tr/" target="_blank">Görüntü İşleme Araştırma Laboratuvarı</a></li>
+						<li><a href="http://ibel.kocaeli.edu.tr/" target="_blank">İnsan Bilgisayar Etkileşimi Araştırma Laboratuvarı</a></li>
+						<li><a href="http://yapbenzet.kocaeli.edu.tr/" target="_blank">Yapay Zeka ve Benzetim Sistemleri Araştırma Laboratuvarı</a></li>
+						<li><a href="projeler.php">Projeler</a></li>
+					  </ul>
+					</li>
+					<li class="dropdown">
+					  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Personel<span class="caret"></span></a>
+					  <ul class="dropdown-menu" role="menu">
+						<li><a href="kadro.php">Öğretim Üyeleri</a></li>
+						<li><a href="ogretimelemanlari.php">Öğretim Elemanları</a></li>
+						<li><a href="idari.php">İdari Personel</a></li>
+					    <!--<li><a href="sistemegiris.php">Sisteme Giriş</a></li> -->
+					  </ul>
+					</li>
+					<li class="dropdown">
+					  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Lisans<span class="caret"></span></a>
+					  <ul class="dropdown-menu" role="menu">
+						<li><a href="http://mf.kocaeli.edu.tr/ogrenci/formlar.php" target="_blank">Öğrenci Dilekçe ve Formları</a></li>
+						<li><a href="lisansdersplani.php">Ders Planı</a></li>
+						<li><a href="lisansdersicerikleri.php">Ders İçerikleri</a></li>
+  					    <li><a href="files\208_Ders_Programi.zip">Ders Programı</a></li>
+						<li><a href="files\2016-2017_Danismanliklari.pdf" target="_blank">Akademik Danışmanlar</a></li>
+						<li><a href="#">İntibak Öğrencileri</a></li>
+						<li><a href="http://www.kocaeli.edu.tr/tanitim/" target="_blank">Aday Öğrenciler</a></li>
+						<li><a href="projebitirmeler.php">Araştırma Problemleri ve Bitirme</a></li>
+						<li><a href="http://odb.kocaeli.edu.tr/akademik_takvim.php" target="_blank">Akademik Takvim</a></li>
+						<li><a href="mudek.php">MÜDEK</a></li>
+						<li><a href="https://ogr.kocaeli.edu.tr/KOUBS/Genel/Diplomasorgu/diplomanosorgu.cfm" target="_blank">Diploma Sorgulama</a></li>						
+						<li><a href="http://mf.kocaeli.edu.tr/ogrenci/staj.php" target="_blank">Staj</a></li>						
+								
+					  </ul>
+					</li>
+					<li class="dropdown" >
+					  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Y.Lisans/Doktora <span class="caret"></span></a>
+					  <ul class="dropdown-menu" role="menu">
+						<li><a href="calismaalanlari.php">Çalışma Alanları</a></li>
+						<li><a href="fbederslistesi.php" >Ders Listesi</a></li>
+						<li><a href="files\2016-2017_Guz_Lisansustu.pdf" target="_blank">Ders Programı</a></li>
+						<li><a href="#">Tezler</a></li>
+
+					  </ul>
+					</li>
+					</li>
+					<li>
+					  <a href="iletisim.php">İletişim</a>
+					</li>
+
+				</div><!-- /.navbar-collapse -->
+			  </div><!-- /.container-fluid -->
+			</nav>
+		</div>
+	</div>
+      </div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-3">
+      <a href="http://www.kocaeli.edu.tr" target="_blank"><img src="images/logo.png"/></a>
+      <hr />	
+      <div class="list-group">
+            <ul class="liste">
+              <li class="yazi"><a href="http://kutuphane.kocaeli.edu.tr/anasayfa.php" class="" target="_blank">KÜTÜPHANE</a></li>
+              <li class="yazi"><a href="http://uzem.kocaeli.edu.tr/uzemweb/" class="" target="_blank">KOU UZEM</a></li>
+              <li class="yazi"><a href="http://edestek.kocaeli.edu.tr/login/index.php" class="" target="_blank">KOU E-DESTEK</a></li>
+			  <li class="yazi"><a href="http://ogr.kocaeli.edu.tr/KOUBS/Istatistik/index.cfm" target="_blank">KOU İSTATİSTİK</a></li>
+			  <li class="yazi"><a href="files\208_Ders_Programi.zip" class="" target="_blank">DERS PROGRAMI</a></li>
+              <li class="yazi"><a href="http://odb.kocaeli.edu.tr/akademik_takvim.php" target="_blank" class="">AKADEMİK TAKVİM</a></li>
+			  <li class="yazi"><a href="http://odb.kocaeli.edu.tr/" target="_blank" class="">ÖĞRENCİ İŞLERİ</a></li>
+            </ul>
+	</div>
+   </div>
+    <div class="col-md-9" >
+        <div class="col-xs-2">
+           <img src="images/line.png" class="line" />
+        </div>
+      <div class="bs-example" >
+   		 <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Carousel indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>   
+            <!-- Wrapper for carousel items -->
+      
+		<div class="carousel-inner">
+            <div class="item active">
+                <img src="images/1.jpg"  alt="First Slide">
+            </div>
+            <div class="item">
+                <img src="images/2.jpg" alt="Second Slide">
+            </div>
+            <div class="item">
+                <img src="images/3.jpg" alt="Third Slide">
+            </div>
+        </div>
+        <!-- Carousel controls -->
+        <a class="carousel-control left" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class="carousel-control right" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+        </a>
+    		</div>
+		</div>
+    </div>
+  </div>
+</div>
+<br /><br />
+<div class="container-fluid" role="main" >
+			<div class="col-sm-8 col-sm-offset-2">
+					
+					<center><h3>BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ ÖĞRETİM ÜYELERİNCE YÜRÜTÜLEN PROJELER</h3></center>	
+					<hr>
+					<p class="text-center"><br>
+				    <li><strong>2016</strong></li>
+				    <ul>
+				      <li>"Hastanenin verimliliğini arttırmak amacıyla bina içinde konum tespiti sisteminin geliştirilmesi" (2016-...), Kocaeli Üniversitesi  Bilimsel Araştırmalar Projeleri (BAP) Destek Fonu, Proje Yürütücüsü: Yrd. Doç. Dr. Suhap Şahin</li>
+				      <li>"Kullanılabilir Mobil Form Tasarımı için Kılavuz Geliştirme" (2016-...), Kocaeli Üniversitesi  Bilimsel Araştırmalar Projeleri (BAP) Destek Fonu, Proje Yürütücüsü: Yrd. Doç. Dr. Pınar Onay Durdu</li>
+				      <li>"Parça Uydu Görüntülerinden Nesne Çıkarımı Için Esle-Indirge Tabanlı Dagıtık Ve Ölçeklenebilir Büyük-Veri Isleme Mimarisi" (2016-…), TÜBİTAK Araştırma Destek Programları Başkanlığı (ARDEB) 3001,  Proje Yürütücüsü: Doç. Dr. Ahmet SAYAR</li>
+				      <li>"Mobese Kameraları Üzerinden Dağıtık Mesajlaşma Protokolüyle Gerçek Zamanlı Nesne Sınıflandıran Akıllı Trafik Sistemi" (2016-...), Kocaeli Üniversitesi  Bilimsel Araştırmalar Projeleri (BAP) Destek Fonu, Proje Yürütücüsü: Doç. Dr. Ahmet SAYAR</li>
+					<br>
+			      </ul>
+				    <li><strong>2014</strong></li>
+				    <ul>
+				      	<li>”Müzeler İçin Akıllı Takip Sistemi” (2014-…),  TÜBİTAK Araştırma Destek Programları Başkanlığı (ARDEB) 1005,&nbsp; Proje Yürütücüsü: Yrd. Doç. Dr. Suhap Şahin</li>
+			          	<li>”Ürünlerin Zayıf Özelliklerinin Türkçe Kullanıcı&nbsp;Yorumlarından Özellik Tabanlı Sentiment ile Keşfedilmesi” (2014-…),  TÜBİTAK Araştırma Destek Programları Başkanlığı (ARDEB) 3001,&nbsp; Proje Yürütücüsü: Yrd. Doç. Dr. Sevinç İlhan Omurca </li>
+				    </ul>
+			      </ul>
+			      <br>
+		
+			
+				    <li><strong>2013</strong></li>
+				    <ul>
+				      <li>”Yerel Bulut Sunucu Tabanlı Etkin Bir  Veri Saklama ve İçerik Dağıtımı Yönetimi Sistemi” (2013-…), TÜBİTAK Araştırma Destek Programları Başkanlığı (ARDEB) 1003,&nbsp;Proje Yürütücüsü: Prof. Dr. Adnan Kavak</li>
+			           </ul>
+			            <br>
+				  
+				    <li><strong>2012</strong></li>
+				    <ul>
+				      <li>“Mobil Ad-Hoc Ağlar İçin LTE Tabanlı  Görüntü Aktarımı” (2012-2013), 
+				        EUREKA Projesi, Proje Danışmanı: Prof. Dr. Adnan Kavak</li>
+			           </ul>
+				    <ul>
+				      <li>“Eş Zamanlı El Yazısı ve Kullanıcı  Tanıma” (2012-2014), Proje Yürütücüsü: Nevcihan DURU </li>
+			           </ul>
+				    <ul>
+				      <li>Uydu Mozaik Ada Görüntüntülerinden Adaların Tespiti ve Yüksek Başarımlı Zaman-Mekansal Analizler İçin Vektörel Modellenmesi&quot; (2012-...), Kocaeli Üniversitesi&nbsp; Bilimsel Araştırmalar  Projeleri (BAP) Destek Fonu, Proje Yürütücüsü: Yrd. Doç. Dr. Ahmet SAYAR</li>
+			            </ul>
+			     
+				  <ul>
+				    <li>“Taktik Ortak Destek Sistemi” (2012-2013),  Savunma Sanayi Müsteşarlığı, MODSIMMER, Proje Araştırmacısı: Yrd. Doç. Alev  Mutlu</li>
+			         </ul>
+			      
+			
+				  <br>
+				    <li><strong>2011</strong></li>
+				    <ul>
+				      	<li>“Veri Madenciliği ve Görüntü İşleme  Tekniklerinin Tıbbi Görüntülere Uygulanması”(2011-2013),  Proje Yürütücüsü: Nevcihan DURU</li>
+			          	<li>”Etkin Üretim Miktarı ve Emniyet Stoğu Seviyesinin Belirlenmesinde Kullanılacak Yeni Bir Yöntem” (2011-2013),  Kocaeli Üniversitesi&nbsp; Bilimsel Araştırmalar  Projeleri (BAP) Destek Fonu,&nbsp; Proje Yürütücüsü: Yrd. Doç. Dr. Sevinç İlhan Omurca</li>
+				    </ul>
+			     
+			
+				  <br>
+				    <li><strong>2009</strong></li>
+				    <ul>
+				      <li>“Kablosuz Algılayıcı Ağlarda Akıllı  Anten Kullanan Sektörel Tarama Tabanlı Konum Tahmini: OPNET ile Simülasyon  Çalışması” (2009-2010),&nbsp;TÜBİTAK Araştırma Destek Programları Başkanlığı (ARDEB) 1002, Proje Yürütücüsü: Adnan  Kavak</li>
+			        </ul><br>
+				    <li><strong>2005</strong></li>
+				    <ul>
+				      <li>“MIMO Sistemler için RF/IF Alıcı-Verici  Ünitesi Geliştirilmesi” (2005-2007), Kocaeli Universitesi Bilimsel Araştırmalar  Birimi, Proje Yürütücüsü: Adnan Kavak </li>
+			           </ul><br>
+				   <li><strong>2004</strong></li>
+				    <ul>
+				      <li>“Kampus Intranet Sistemi İçin Kablosuz  Yerel Alan Ağı Tabanlı Güvenlik Ortam Erişim Kontrolu Uygulaması” (2004-2006), Bilimsel  Araştırma ve Geliştirme için Altyapıyı Destekleme Programı Projeleri, Proje  Yürütücüsü: Nevcihan DURU</li>
+			           </ul><br>
+				    <li><strong>2002</strong></li>
+			      	<ul>
+					<li>“Üçüncü Nesil Kablosuz Haberleşme  Sistemleri için Smart Anten Algoritması Geliştirilmesi ve Prototip Üzerinde  Uygulanması” (2002-2004), TÜBİTAK Araştırma Destek Programları Başkanlığı (ARDEB) 1001 ve KOÜ-2002/40, Proje  Yürütücüsü: Adnan Kavak</li>
+			      	</ul>
+				  
+					</p><br><br><br><br><br><br>			
+				</div><!-- /.col -->
+			
+			
+			
+		</div><!-- /.conainer -->
+
+
+
+    <footer>
+    	<div class="container">
+        	<div class="row">
+            	<div class="col-md-4">
+					<h4>İstek, Görüş ve Öneriler<hr style="margin-top:2px" /></h4>
+					<p align="center">Designed & Produced  <br> Emre Ozan Öztürk - İbrahim Kangal - Kutluhan Algür <br> 2016</p>
+                     <!-- <a href="http://akademikpersonel.kocaeli.edu.tr/binner/">A. Burak İNNER</a> / <a href="http://akademikpersonel.kocaeli.edu.tr/hikmetcan.ozcan/">Hikmetcan ÖZCAN </a></p> -->
+                </div>
+                <div class="col-md-4">
+                <h4>Sık Kullanılanlar<hr style="margin-top:2px" /></h4>
+                    <ul class="list-unstyled">
+						<li class="altyazi"><a href="http://kutuphane.kocaeli.edu.tr/anasayfa.php" target="_blank" style="text-decoration: none; color: #fff">Kütüphane</a></li>
+                    	 <li class="altyazi"><a href="http://ogr.kocaeli.edu.tr/KOUBS/ogrenci/index.cfm" target="_blank" style="text-decoration: none; color: #fff">Öğrenci Bilgi Sistemi</a></li>
+                          <li class="altyazi"><a href="http://ogr.kocaeli.edu.tr/KOUBS/AkaPers/index.cfm" target="_blank" style="text-decoration: none; color: #fff">Akademik Personel Sistemi</a></li>
+                          <li class="altyazi"><a href="http://ogr.kocaeli.edu.tr/KOUBS/Idaripers/anagiris.cfm" target="_blank" style="text-decoration: none; color: #fff">İdari Personel Sistemi</a></li>
+                          
+                    </ul>
+                </div>
+
+                <div class="col-md-4">
+                                <h4>İletişim<hr style="margin-top:2px" /></h4>
+                <p><strong>Adres:</strong> KOÜ Mühendislik Fakültesi (B Blok) Bilgisayar Mühendisliği Umuttepe Yerleşkesi 41380 Kocaeli<br/>
+                <strong>Tel:</strong> +90 (262) 303 35 60<br />
+				<strong>E-Posta:</strong> bilgisayar@kocaeli.edu.tr<br />
+				<strong>Web:</strong> http://bilgisayar.kocaeli.edu.tr </p>
+                </div>
+                
+                 
+            </div>
+        </div>
+    
+    </footer>
+
+</body>
+</html>
